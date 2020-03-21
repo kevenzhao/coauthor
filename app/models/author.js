133f5c81b6dfbfa740fa31d3c6b66f79376371b0
Keven Zhao <keven.zhao@mail.utoronto.ca>
2020-03-21 00:22:20 -0400

    Added sequelized information for the database in author.js
    Specifically the following 3 tables:
    
    - authors
    - coauthors
    - todo_list

--- app/models/author.js
+++ app/models/author.js
@@ -1,12 +1,47 @@
 module.exports = (sequelize, type) => {
-  return sequelize.define('author', {
+  return sequelize.define('authors', {
     id: {
       type: type.INTEGER,
       primaryKey: true,
       autoIncrement: true
     },
     name: type.STRING,
-    email: type.STRING,
-    scholar_id: type.STRING,
+    gs_profile_id: {
+      type: type.STRING,
+      unique: true
+    },
+    affiliation: type.STRING,
+    interest: type.STRING,
+  })
+}
+
+module.exports = (sequelize, type) => {
+  return sequelize.define('coauthors', {
+    id: {
+      type: type.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    person1_id: {
+      type: type.INTEGER,
+      references: 'authors',  // Foreign Key Table Name
+      referencesKey: 'id'     // Foreign Key Column Name
+    },
+    person2_name: type.STRING,
+    most_recent_year: type.INTEGER,
+    most_recent_paper: type.STRING,
+  })
+}
+
+module.exports = (sequelize, type) => {
+  return sequelize.define('todo_list', {
+    id: {
+      type: type.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: type.STRING,
+    type: type.STRING,
+    status: type.INTEGER,
   })
 }
